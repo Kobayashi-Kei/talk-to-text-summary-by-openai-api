a452903a6b49a3e046e0197c3a83bca8c2e4838a
K-Kobayashi <k1810249@uec.ac.jp>
2023-08-17 18:53:03 +0900

    READMEを更新

--- README.md
+++ README.md
@@ -1,22 +1,30 @@
-# talk-to-text-summary-by-openai-api 
-OpenAI Whisperを使ってmp3データを文字起こし，ChatGPTで整形，要約するプログラムです．
+# talk-to-text-summary-by-openai-api
+
+OpenAI Whisper を使って mp3 データを文字起こし，ChatGPT で整形，要約するプログラムです．
 
 ## requirements
-- openai
-- subprocess
-- pydub
-- ffmpeg
-  - mac os
-```
-brew install ffmpeg
-```
 
-## 参考リンク
+- ffmpeg
+  - mac os で homebrew が入っている場合: `brew install ffmpeg`
+- pip
+  - openai
+  - subprocess
+  - pydub
+  - ffmpeg
+
+## 実行手順
+
+1. OpenAI API Key を 記入した，apikey.txt ファイルを作成．
+2. `mkdir data`
+3. `mkdir data/voice_data`
+4. `data/voice_data` に mp3 または m4a ファイルを設置
+5. `transcript.py` の `dirPath=` を "voice_data"，`fileName=`を 3. で設置した音声ファイルの名前にする．（拡張子含む，例 hogehoge.mp3
+6. `python3 transcript.py`
+
+## 実装の参考リンク
+
 https://openai.com/blog/introducing-chatgpt-and-whisper-apis
 
 https://www.watch.impress.co.jp/docs/topic/1493886.html
 
 https://zenn.dev/k_kind/articles/chatgpt-api-q-and-a
-
-
-
--- transcript.py
+++ transcript.py
@@ -8,12 +8,10 @@ import ffmpeg
 openai.api_key_path = 'apikey.txt'
 
 def main():    
-    # 入力する音声ファイル
-    dirPath = "data/jsai/"
-    # dirPath = "data/sample/"
-    fileName = 'JSAI_ポスター発表.mp3'
-    # fileName = 'sampleSuper.mp3'
-    
+    # 入力
+    dirPath = ""
+    fileName = ""
+
     # m4aファイルをmp3に変換
     if "m4a" in  fileName:
         m4aToMp3(dirPath, fileName)
@@ -22,55 +20,79 @@ def main():
     # 音声ファイルを分割
     splitVoiceFile(dirPath, fileName)
     
-    # textファイル用のディレクトリを作成
+    # 出力先ディレクトリを作成
     if os.path.exists(dirPath + "text") == False:
         os.mkdir(dirPath + "text")
-        
-    # chunkファイルのリストを習得
+    if os.path.exists(dirPath + "output") == False:
+        os.mkdir(dirPath + "output")
+
+    # Whisperで文字起こし
     chunkList = os.listdir(dirPath + "chunk/")
     for chunk in chunkList:
+        if os.path.exists(dirPath + "text/" + chunk.replace(".mp3", ".txt")):
+            continue
+        print("Whisperで文字起こし中:" + chunk)
         audio_chunk = open(dirPath + "chunk/" + chunk, "rb")
         transcript = openai.Audio.transcribe("whisper-1", audio_chunk)
         txt = transcript['text']
         f = open(dirPath + "text/" + chunk.replace(".mp3", "") + ".txt", "w")
         f.write(txt)
         f.close()
+        
     
-    # テキストファイルを結合 & GPTで整形
+    # テキストファイルを結合 & ChatGPTでトピック抽出
     txtList = os.listdir(dirPath + "text/")
     for txt_file in txtList:
         f = open(dirPath + "text/" + txt_file, "r")
         txt = f.read()
         f.close()
-        
-        # output.txtに文字起こし結果を書き込み
-        f = open(dirPath + "output.txt", "a")
+        f = open(dirPath + "output/output.txt", "a")
         f.write(txt)
         f.close()
-        
-        # GPTで整形    
+
+        # ChatGPTでトピック抽出
+        print("ChatGPTでトピック抽出中:" + txt_file)
         txt_chunks = split_string(txt)
-        result_txt = ""
+        topic_txt = ""
         for txt_chunk in txt_chunks:
             response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
                 messages=[
-                    {"role": "system", "content": "あなたは優秀な文字起こし音声の修正者です．"},
-                    {"role": "assistant", "content": txt_chunk},
-                    {"role": "user", "content": "この文章はspeechToTextで変換したものです．誤字脱字をできる限り修正して読みやすい文章にしてください．"},
+                    {"role": "user", "content": "以下のテキストから主要なトピックを箇条書きで列挙してください．それぞれの項目単体で理解できるように内容を省略せずに書いてください．\n以下がテキストです：\n" + txt_chunk },
                 ]   
             )
-            # print(response["choices"][0]["message"]["content"])
-            result_txt += response["choices"][0]["message"]["content"]
+            topic_txt += response["choices"][0]["message"]["content"]
         
-        f = open(dirPath + "text/" + txt_file.replace(".txt", "_gpt.txt"), "a")
-        f.write(result_txt)
+        f = open(dirPath + "text/" + txt_file.replace(".txt", "_topic.txt"), "a")
+        f.write(topic_txt)
         f.close()
+        f = open(dirPath + "output/output_topic.txt", "a")
+        f.write(topic_txt)
+        f.close()
+
+
+        # 以下は効果が薄いのでやらない    
+        # # GPTで整形    
+        # txt_chunks = split_string(txt)
+        # result_txt = ""
+        # for txt_chunk in txt_chunks:
+        #     response = openai.ChatCompletion.create(
+        #         model="gpt-3.5-turbo",
+        #         messages=[
+        #             {"role": "user", "content": "以下の文章はspeechToTextで変換したものです．誤字脱字をできる限り修正して読みやすい文章にしてください．" + "\n" + txt_chunk },
+        #         ]   
+        #     )
+        #     # print(response["choices"][0]["message"]["content"])
+        #     result_txt += response["choices"][0]["message"]["content"]
         
-        # output.txtに文字起こし &　GPTでの整形結果を書き込み
-        f = open(dirPath + "output_gpt.txt", "a")
-        f.write(result_txt)
-        f.close()
+        # f = open(dirPath + "text/" + txt_file.replace(".txt", "_gpt.txt"), "a")
+        # f.write(result_txt)
+        # f.close()
+        
+        # # output.txtに文字起こし &　GPTでの整形結果を書き込み
+        # f = open(dirPath + "output_gpt.txt", "a")
+        # f.write(result_txt)
+        # f.close()
     
 
 def m4aToMp3(dirPath, fileName):
